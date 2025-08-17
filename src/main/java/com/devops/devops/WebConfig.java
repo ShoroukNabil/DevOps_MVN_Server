c10dddf9dbbdce605b4bb06bf3158d47f888f0d3
snabil <snabil@kn-it.com>
2025-08-17 10:05:54 +0300

    Fix WebConfig

--- src/main/java/com/devops/devops/WebConfig.java
+++ src/main/java/com/devops/devops/WebConfig.java
@@ -7,14 +7,21 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                // Allow local dev & production domains
+//                .allowedOriginPatterns(
+//                        "http://localhost:*",        // Angular local dev
+//                        "http://127.0.0.1:*",        // Localhost via IP
+//                        "http://10.10.10.131:*",     // Your LAN IP
+//                        "http://angular-app:*",      // Docker/compose Angular
+//                        "https://your-production-domain.com" // Production
+//                )
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowCredentials(true);
         registry.addMapping("/**")
-                // Allow local dev & production domains
-                .allowedOriginPatterns(
-                        "http://localhost:*",        // Angular local dev
-                        "http://127.0.0.1:*",        // Localhost via IP
-                        "http://10.10.10.131:*",     // Your LAN IP
-                        "http://angular-app:*",      // Docker/compose Angular
-                        "https://your-production-domain.com" // Production
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "http://127.0.0.1:4200"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
